2cb93263eacdc68d1269632ddef326431a68dbe5
skumar <skumar@monarchtractor.com>
2025-02-01 22:48:36 +0000

    fix: fix evalaute.py path

--- bash/evaluate.sh
+++ bash/evaluate.sh
@@ -3,6 +3,6 @@
 rm -rf logs/evaluate.log
 exec >> logs/evaluate.log 2>&1
 
-python evaluate_model.py \
+python evaluate.py \
     --config configs/evaluate.yaml \
     --color_map configs/Mavis.yaml 
