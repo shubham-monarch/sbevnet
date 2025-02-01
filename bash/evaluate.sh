69d6078cada9c4b4a5cf4c8e4aa0eea9c027b320
skumar <skumar@monarchtractor.com>
2025-02-01 08:03:46 +0000

    feat: udpate bash / config files

---
+++ bash/evaluate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+exec >> logs/evaluate.log 2>&1
+
+python evaluate_model.py \
+    --config configs/evaluate.yaml \
+    --color_map configs/Mavis.yaml 
--- bash/train-dist.sh
+++ bash/train-dist.sh
@@ -2,3 +2,5 @@
 exec >> logs/main.log 2>&1
 
 python train-dist.py --config configs/train-dist.yaml
+
+# python train-dist.py --config configs/train-dist.yaml --resume checkpoints/best_model.pth
